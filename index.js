09a43f57a9b9bf73e4cf15e1037e8f3b53116226
Aineen0626 <169159845+Aineen0626@users.noreply.github.com>
2024-05-18 13:35:26 -0400

    js file added

---
+++ index.js
@@ -0,0 +1,56 @@
+// ---------------------------TASK 1-------------------------------
+
+
+//DECLARE THREE VARIABLES//
+let a = 3;
+let b = 5;
+let c;
+
+//DECLARE EXPERIMENT//
+let experiment = `[
+    let a = 3;
+    let b = 5;
+    let c;
+    ------------
+    a + b = ${a + b};
+    a - b = ${a - b};
+    a * b = ${a * b};
+    a / b = ${a / b};
+    a % b = ${a % b};
+    a += b = ${a += b};
+    a -= b = ${a -= b};
+    a *= b = ${a *= b};
+    a /= b = ${a /= b};
+    a %= b = ${a %= b};
+    a == b = ${a == b};
+    a != b = ${a != b};
+    a > b = ${a > b};
+    a < b = ${a < b};
+    !a && !c = ${!a && !c};
+    !a || !c = ${!a || !c};
+]`;
+
+//PRINT//
+console.log(experiment);
+
+
+
+// ---------------------------TASK 2-------------------------------
+
+// Declare variable first_name
+let first_name = "Aineen";
+
+// Declare variable last_name
+let last_name = "Shukoor";
+
+// Declare variable email
+let email = "abdu0626@algonquinlive.com";
+
+// Declare variable output
+                  // INTERPOLATION 
+let output =  `My name is ${first_name} ${last_name}. You can contact me at ${email}.`;
+
+// print:
+console.log(output);
+
+
